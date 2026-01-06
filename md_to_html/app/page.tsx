16c4d9d3ebda6fa810c4f8fa74d0ff75b24567ce
NeilJoseph019 <neiljoseph.09@gmail.com>
2026-01-06 19:47:14 +0000

    Simple dashboard made using shadcn

--- md_to_html/app/page.tsx
+++ md_to_html/app/page.tsx
@@ -1,9 +1,98 @@
-import Image from "next/image";
+'use client'
+
+import Image from "next/image"
+import { AppSidebar } from "./(components)/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
+        <SidebarProvider
+          style={
+            {
+              "--sidebar-width": "19rem",
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-4"
+              />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="#">
+                      Building Your Application
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </header>
+            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+              <div className="p-5 bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" >
+              <h2 className="text-2xl">Journals</h2>
+              <div className="flex justify-center items-center min-h-[60%]">
+              {
+                [1, 2, 3].map((num, index) => (
+                  <Card
+                  key={index}
+                  className="m-2 w-[25%] h-[30%] bg-gray-100 border-[1.8px] shadow-sm hover:bg-amber-50"
+                  >
+                    <CardHeader>
+                      <CardTitle>Card Title</CardTitle>
+                      <CardDescription>Card Description</CardDescription>
+                      <CardAction>Card Action</CardAction>
+                    </CardHeader>
+                    <CardContent>
+                      <p>Card Content</p>
+                    </CardContent>
+                    <CardFooter>
+                      <p>Card Footer</p>
+                    </CardFooter>
+                  </Card>
+                ))
+              }
+              </div>
+              </div>
+              <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+              </div>
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
         
       </main>
     </div>
